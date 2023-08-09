04af5b48cd7a2a1f2798a48ec534324ba5dccc6c
Jaxel Rojas Lopez <jrojaslopez@microsoft.com>
2023-08-09 16:49:47 -0400

    Added custom assert logic

---
+++ UnitTest/Asserts/CustomAssert.cs
@@ -0,0 +1,21 @@
+﻿using Xunit.Sdk;
+
+
+public class CustomAssert : Assert
+{
+    public static void IsEven( int value)
+    {
+        if (value % 2 != 0)
+            throw IsEvenException.ForNonEvenValue(value);
+    }
+}
+
+public class IsEvenException : XunitException
+{
+    IsEvenException(string message)
+        : base(message)
+    { }
+
+    public static IsEvenException ForNonEvenValue(int value) =>
+        new($"The value {value} was not even.");
+}
