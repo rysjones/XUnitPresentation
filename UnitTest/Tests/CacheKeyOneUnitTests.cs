4de20257843ab4679e6353beb3ad097fa3b0d4c4
Jaxel Rojas Lopez <jrojaslopez@microsoft.com>
2023-08-09 22:57:09 -0400

    Added Cached unit tests to show functionality of shared context between classes

---
+++ UnitTest/Tests/CacheKeyOneUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using UnitTestingProject.TestFixtures;
+
+namespace UnitTestingProject;
+//dotnet test --filter "Category=Cache"
+
+[Collection("CacheCollection")]
+[Trait("Category", "Cache")]
+public class CacheKeyOneUnitTests
+{
+    private readonly CacheService _cacheService;
+
+    public CacheKeyOneUnitTests(CacheFixture context)
+    {
+        _cacheService = context.CacheService;
+    }
+
+    [Fact]
+    public void Get_ReturnsValue_WhenKeyExists()
+    {
+        // Arrange
+        int key = 1;
+        string expectedValue = "One";
+
+        // Act
+        string actualValue = _cacheService.Get(key);
+
+        // Assert
+        Assert.Equal(expectedValue, actualValue);
+    }
+
+    [Fact]
+    public void Exists_ReturnsTrue_WhenKeyExists()
+    {
+        // Arrange
+        int key = 1;
+
+        // Act
+        bool exists = _cacheService.Exists(key);
+
+        // Assert
+        Assert.True(exists);
+    }
+}
---
+++ UnitTest/Tests/CacheManyKeysUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using UnitTestingProject.TestFixtures;
+
+namespace UnitTestingProject;
+//dotnet test --filter "Category=Cache"
+
+[Collection("CacheCollection")]
+[Trait("Category", "Cache")]
+public class CacheManyKeysUnitTests
+{
+    private readonly CacheService _cacheService;
+
+    public CacheManyKeysUnitTests(CacheFixture context)
+    {
+        _cacheService = context.CacheService;
+    }
+
+    [Fact]
+    public void Count_ReturnsNumberOfKeyValuePairs()
+    {
+        // Arrange
+        int expectedCount = 3;
+
+        // Act
+        int actualCount = _cacheService.Count();
+
+        // Assert
+        Assert.True(actualCount >= expectedCount);
+    }
+
+    [Fact]
+    public void Add_AddsKeyValuePair_WhenKeyDoesNotExist()
+    {
+        // Arrange
+        int key = 4;
+        string value = "Four";
+
+        // Act
+        _cacheService.Add(key, value);
+
+        // Assert
+        Assert.Equal(value, _cacheService.Get(key));
+    }
+
+    [Fact]
+    public void Update_UpdatesKeyValuePair_WhenKeyExists()
+    {
+        // Arrange
+        int key = 1;
+        string value = "New One";
+
+        // Act
+        _cacheService.Update(key, value);
+
+        // Assert
+        Assert.Equal(value, _cacheService.Get(key));
+    }
+}
